ae337f21e08a6ef491c259d181073a165e7ca724
Lazar Stijakovic <zomlaaa95 AT gmail DOT com>
2017-05-14 12:48:21 +0200

    Created reposiotories for last 5 entities

---
+++ src/main/java/com/eEducation/ftn/repository/StudentAttendsCourseRepository.java
@@ -0,0 +1,9 @@
+package com.eEducation.ftn.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.eEducation.ftn.model.StudentAttendsCourse;
+
+public interface StudentAttendsCourseRepository extends JpaRepository<StudentAttendsCourse, Long>{
+
+}
